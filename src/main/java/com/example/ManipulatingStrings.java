1884c6aa243bd0c8335e45ebb2a47465cf0bffd8
phoebecd <87622936+phoebecd@users.noreply.github.com>
2024-12-14 01:12:53 -0500

    switchPhrase

--- src/main/java/com/example/ManipulatingStrings.java
+++ src/main/java/com/example/ManipulatingStrings.java
@@ -1,9 +1,15 @@
 package com.example;
 import java.util.Scanner;
 
+// Give students a specified input and outputh they're supposed to get
 public class ManipulatingStrings
 {
-    String phrase = new String ("This is a String test.");
+    //ManipulatingStrings manipulator = new ManipulatingStrings();
+    String phrase = new String ("cupcakes"); 
+    int phraseLength = phrase.length();
+    int middleIndex = phraseLength/2;
+    String firstHalf;
+    String secondHalf;
     public static void main (String[] args)
     {
         
@@ -11,11 +17,18 @@ public class ManipulatingStrings
 
     }
 
-    public String findMiddle3(String p) {
-        //int phraseLength = p.length();;   // number of characters in the phrase String
-        //int middleIndex = phraseLength / 2;;
+    public String findMiddle3() {
         String middle3 = ""; 
-        //middle3 = p.substring(middleIndex-1, middleIndex+2);
+        //phraseLength = p.length();
+        //middleIndex = phraseLength/2;
+        middle3 = phrase.substring(middleIndex-1, middleIndex+2);
         return middle3;
+    }
+
+    public String switchPhrase() {
+        firstHalf = phrase.substring(0,middleIndex);
+        secondHalf = phrase.substring(middleIndex, phraseLength);
+        String switchedPhrase = secondHalf.concat(firstHalf);
+        return switchedPhrase;
     }
 }
--- src/test/java/com/example/ManipulatingStringsTest.java
+++ src/test/java/com/example/ManipulatingStringsTest.java
@@ -1,26 +1,37 @@
 package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+
+//import java.util.List;
 
 public class ManipulatingStringsTest {
-    String phrase = new String ("This is a String test.");
-    int phraseLength;   // number of characters in the phrase String
-    int middleIndex;    // index of the middle character in the String
-    String firstHalf;   // first half of the phrase String
-    String secondHalf;  // second half of the phrase String
-    String switchedPhrase; // a new phrase with original halves switched
-    String middle3;        // stores the middle three characters of the String
-    String city;
-    String state;
+    private ManipulatingStrings main = new ManipulatingStrings();
 
+    @BeforeEach
+    public void setUp() {
+        main = new ManipulatingStrings();
+    }
   
     @Test
     public void testMiddle3() {
         ManipulatingStrings main = new ManipulatingStrings();
-        assertEquals("cak", main.findMiddle3("cupcakes"), "Error calculating middle 3 characters.");
-        assertEquals("and", main.findMiddle3("candy"), "Error calculating middle 3 characters.");
+        assertEquals("cak", main.findMiddle3(), "Error calculating middle 3 characters.");
+       // assertEquals("and", main.findMiddle3("candy"), "Error calculating middle 3 characters.");
     }
+
+    @Test
+    public void testSwitchPhrase() {
+        ManipulatingStrings main = new ManipulatingStrings();
+        assertEquals("akescupc", main.switchPhrase(), "Error switching first and second half.");
+        //assertEquals("ndyca", main.switchPhrase("candy"), "Error switching first and second half.");
+    }
+
+
 
 }
