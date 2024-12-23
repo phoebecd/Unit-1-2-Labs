150dbcc5f5b1f7d439e0e34b5cdbd42f495bf251
phoebecd <87622936+phoebecd@users.noreply.github.com>
2024-12-23 16:22:26 -0500

    12/23-Phrase Edits

--- src/main/java/com/example/ManipulatingStrings.java
+++ src/main/java/com/example/ManipulatingStrings.java
@@ -5,30 +5,48 @@ import java.util.Scanner;
 public class ManipulatingStrings
 {
     //ManipulatingStrings manipulator = new ManipulatingStrings();
+    int phraseLength;
+    int middleIndex;
     String phrase = new String ("cupcakes"); 
-    int phraseLength = phrase.length();
-    int middleIndex = phraseLength/2;
-    String firstHalf;
-    String secondHalf;
+    String switchedPharse;
+
     public static void main (String[] args)
     {
-        
-               
+                  
 
     }
 
-    public String findMiddle3() {
+    public String findMiddle3(String p) {
         String middle3 = ""; 
-        //phraseLength = p.length();
-        //middleIndex = phraseLength/2;
-        middle3 = phrase.substring(middleIndex-1, middleIndex+2);
+        phraseLength = p.length();
+        middleIndex = phraseLength/2;
+        middle3 = p.substring(middleIndex-1, middleIndex+2);
         return middle3;
     }
 
-    public String switchPhrase() {
-        firstHalf = phrase.substring(0,middleIndex);
-        secondHalf = phrase.substring(middleIndex, phraseLength);
+    public String switchPhrase(String p) {
+        findMiddle3(p);
+        String firstHalf = p.substring(0,middleIndex);
+        String secondHalf = p.substring(middleIndex, phraseLength);
         String switchedPhrase = secondHalf.concat(firstHalf);
         return switchedPhrase;
+    }
+
+    public String replaceBlanks(String p) {
+        findMiddle3(p);
+        String switchedPhrase = switchPhrase(p).replace(" ", "*");
+        return switchedPhrase;
+    }
+
+    public void printPhraseInfo(String p) {
+        findMiddle3(p);
+        System.out.println();
+        System.out.println("Original phrase: " + p);
+        System.out.println("Length of the phrase: " + phraseLength);
+        System.out.println("Index of the middle: " + middleIndex);
+        System.out.println("Character at the middle index: " + p.charAt(middleIndex));
+        System.out.println("Switched phrase: " + replaceBlanks(p));
+        System.out.println("Middle 3: " + findMiddle3(p));
+        System.out.println();
     }
 }
--- src/test/java/com/example/ManipulatingStringsTest.java
+++ src/test/java/com/example/ManipulatingStringsTest.java
@@ -1,35 +1,40 @@
 package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-
-//import java.util.List;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
 
 public class ManipulatingStringsTest {
     private ManipulatingStrings main = new ManipulatingStrings();
-
-    @BeforeEach
-    public void setUp() {
-        main = new ManipulatingStrings();
-    }
   
     @Test
     public void testMiddle3() {
         ManipulatingStrings main = new ManipulatingStrings();
-        assertEquals("cak", main.findMiddle3(), "Error calculating middle 3 characters.");
-       // assertEquals("and", main.findMiddle3("candy"), "Error calculating middle 3 characters.");
+        assertEquals("ryt", main.findMiddle3("Tarrytown"), "Error calculating middle 3 characters.");
+        assertEquals(" Yo", main.findMiddle3("New York"), "Error calculating middle 3 characters.");
     }
 
     @Test
     public void testSwitchPhrase() {
         ManipulatingStrings main = new ManipulatingStrings();
-        assertEquals("akescupc", main.switchPhrase(), "Error switching first and second half.");
-        //assertEquals("ndyca", main.switchPhrase("candy"), "Error switching first and second half.");
+        assertEquals("ytownTarr", main.switchPhrase("Tarrytown"), "Error switching first and second half.");
+        assertEquals("YorkNew ", main.switchPhrase("New York"), "Error switching first and second half.");
+    }
+
+    @Test
+    public void testReplaceBlanks() {
+        ManipulatingStrings main = new ManipulatingStrings();
+        assertEquals("CS*ClassHello*AP", main.replaceBlanks("Hello APCS Class"), "Error replacing blanks with asterisks.");
+        assertEquals("YorkNew*", main.replaceBlanks("New York"), "Error replacing blanks with asterisks.");
+    }
+
+    @Test
+    public void testPrintPhraseInfo() throws Exception {
+        String text = tapSystemOut(() -> {
+            main.printPhraseInfo("This is a String test.");
+        });
+        String expectedOutput = "\nOriginal phrase: This is a String test.\nLength of the phrase: 22\nIndex of the middle: 11\nCharacter at the middle index: t\nSwitched phrase: tring*test.This*is*a*S\nMiddle 3: Str\n\n";
+        assertEquals(expectedOutput, text);
     }
 
 
